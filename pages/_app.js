ec1695c0137a2359e8ee38782f0ed471d368390a
Abhishek Chatterjee <abhishek.chatterjee@optiphoenix.com>
2023-04-05 10:31:51 +0530

    added banner component

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,6 @@ function MyApp({ Component, pageProps }) {
     return (
         <div>
             <Component {...pageProps} />
-            <footer>
-                <p>© 2023 Abhishek</p>
-            </footer>
         </div>
     );
 }
