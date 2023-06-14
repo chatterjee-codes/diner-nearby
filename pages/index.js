20180f9a4362105ddbc67c8d236143eae4b77793
Abhishek Chatterjee <abhishek.chatterjee@optiphoenix.com>
2023-06-14 10:40:37 +0530

    added coffee stores, unsplash and foursquare api integrated

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,25 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 import Banner from '../components/banner';
+import Card from '../components/card';
+import { fetchCoffeeStores } from '../lib/coffee-stores';
 
-export default function Home() {
+// import coffeeStoresData from '../data/coffee-stores.json';
+
+export async function getStaticProps(context) {
+    // console.log('hi getStaticProps');
+
+    const coffeeStores = await fetchCoffeeStores();
+
+    return {
+        props: {
+            coffeeStores
+        } // will be passed to the page component as props
+    };
+}
+
+export default function Home(props) {
+    // console.log('props', props);
     const handleOnBannerBtnClick = () => {
         console.log('hi! banner button');
     };
@@ -26,6 +43,27 @@ export default function Home() {
                         height={400}
                     />
                 </div>
+                {props.coffeeStores.length > 0 && (
+                    <>
+                        <h2 className={styles.heading2}>Toronto Stores</h2>
+                        <div className={styles.cardLayout}>
+                            {props.coffeeStores.map((coffeeStore) => {
+                                return (
+                                    <Card
+                                        key={coffeeStore.id}
+                                        name={coffeeStore.name}
+                                        imgUrl={
+                                            coffeeStore.imgUrl ||
+                                            'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
+                                        }
+                                        href={`/coffee-store/${coffeeStore.id}`}
+                                        className={styles.card}
+                                    />
+                                );
+                            })}
+                        </div>
+                    </>
+                )}
             </main>
         </div>
     );
